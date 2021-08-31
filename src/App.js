1a64cc49aed4c3dc520a8b15bbf1bfd76689a985
Namdev01 <sunildarji.civ18@iitbhu.ac.in>
2021-08-31 23:27:23 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react'
-
+import { Switch, Route, BrowserRouter } from 'react-router-dom'
+import Home from './Components/Home'
+import Login from './Components/Login'
+import Signin from './Components/Signin'
 const App = () => {
     return (
         <>
-         <div> 
-             hello ji  
-        </div>
+             <BrowserRouter>
+          <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/login" component={Login}/>
+            <Route path="/signin" component={Signin}/>
+          </Switch>
+      </BrowserRouter>
         </>
     )
 }
