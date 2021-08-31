66b7d4c24ef82650e4aaa45ab015f14024185abe
Namdev01 <sunildarji.civ18@iitbhu.ac.in>
2021-09-01 01:48:58 +0530

    database added

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,33 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from "react-router-dom";
 import { Grid, Paper, Avatar, TextField, Button, Typography, FormControlLabel, Checkbox, makeStyles, AccordionSummary } from '@material-ui/core'
 import AccountBoxIcon from '@material-ui/icons/AccountBox';
 import { Instagram, Facebook, GitHub, LinkedIn } from '@material-ui/icons';
 import B2H from './B2H'
 const Login = () => {
-    const { paperStyle, avatarStyle, btnstyle,links, link } = useStyle();
+    const { paperStyle, avatarStyle, btnstyle, links, link } = useStyle();
+    const [user, setUser] = useState({
+        username: "",
+        password: "",
+    })
+    let name, value;
+    const setData = (event) => {
+        name = event.target.name;
+        value = event.target.value;
+        setUser({ ...user, [name]: value });
+    };
+    const submitData = async (e) => {
+        e.preventDefault();
+        const {username,password}=user;
+        const res=await fetch(`${process.env.REACT_APP_FIREBASE_DATABASE_URL}meradata.json`,
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": 'application/json',
+            },
+            body: JSON.stringify({
+                username,
+                password,
+            }),
+        }
+        );
+    };
     return (
         <Grid>
-            <B2H/>
-            <Paper elevation={10} className={paperStyle}>
+            <B2H />
+            <Paper elevation={10} className={paperStyle} method="POST">
                 <Grid align='center'>
                     <Avatar className={avatarStyle}><AccountBoxIcon /></Avatar>
                     <h2>Log In</h2>
                 </Grid>
-                <TextField variant="outlined" label='Username' placeholder='Enter username' fullWidth required />
+                <TextField
+                    variant="outlined"
+                    label='Username'
+                    placeholder='Enter username'
+                    name="username"
+                    value={user.username}
+                    onChange={setData}
+                    fullWidth
+                    required />
                 <br />
                 <br />
-                <TextField variant="outlined" label='Password' placeholder='Enter password' type='password' fullWidth required />
-                <FormControlLabel
-                    control={
-                        <Checkbox
-                            name="checkedB"
-                            color="primary"
-                        />
-                    }
-                    label="Remember me"
-                />
-                <Button type='submit' color='primary' variant="contained" className={btnstyle} fullWidth>Log In</Button>
+                <TextField
+                    variant="outlined"
+                    label='Password'
+                    placeholder='Enter password'
+                    name="password"
+                    value={user.password}
+                    onChange={setData}
+                    type='password'
+                    fullWidth
+                    required />
+                <Button type='submit' color='primary' variant="contained" className={btnstyle} fullWidth onClick={submitData}>Log In</Button>
                 <Typography >
                     <Link href="#" >
                         Forgot password ?
